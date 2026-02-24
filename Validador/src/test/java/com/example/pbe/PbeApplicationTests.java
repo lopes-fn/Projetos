5b911984fab937b14dc5041a90f17d7857d2030a
Vinicius Lopes <viniciuslopesfn@gmail.com>
2026-02-24 17:33:35 -0300

    commit

---
+++ Validador/src/test/java/com/example/pbe/PbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
